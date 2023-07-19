aaf0a6d2f9204358d8ab01f1c400bd95e8825764
ali3030 <ali3030@users.noreply.github.com>
2023-07-19 20:14:06 +0300

    am i dead   part 10 ?

--- app/src/main/java/com/example/pizza/screen/PizzaScreenViewModel.kt
+++ app/src/main/java/com/example/pizza/screen/PizzaScreenViewModel.kt
@@ -1,41 +1,49 @@
 package com.example.pizza.screen
 
 import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.unit.Dp
 import com.example.pizza.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-
 @HiltViewModel
 class PizzaScreenViewModel @Inject constructor():
     BaseViewModel<PizzaScreenState>(PizzaScreenState()) {
-    fun toggleTopping(page: Int, topping: Int) {
-        Log.d("PizzaScreenViewModel", "toggleTopping called with page = $page and topping = $topping")
-        _state.value = _state.value.copy(
-            selectedToppings = _state.value.selectedToppings.toMutableList().apply {
-                this[page] = this[page].toMutableList().apply {
-                    this[topping] = !this[topping]
-                }
-            }
-        )
-        Log.d("PizzaScreenViewModel", "selectedToppings after update = ${_state.value.selectedToppings}")
+
+    private val _pizzaState = mutableStateOf(PizzaScreenState())
+    val pizzaState: State<PizzaScreenState> get() = _pizzaState
+
+    init {
+        _state.value = PizzaScreenState()
     }
 
+    fun toggleTopping(page: Int, topping: Int) {
+        _pizzaState.value = _pizzaState.value.copy(
+            selectedToppings = _pizzaState.value.selectedToppings.toMutableList().also {
+                it[page] = it[page].toMutableList().also { it[topping] = !it[topping] }
+            }
+        )
+        _state.value = _pizzaState.value
+    }
 
     fun changePizzaSize(page: Int, size: Dp) {
-        _state.value = _state.value.copy(
-            pizzaSizes = _state.value.pizzaSizes.toMutableList().apply {
+        _pizzaState.value = _pizzaState.value.copy(
+            pizzaSizes = _pizzaState.value.pizzaSizes.toMutableList().apply {
                 this[page] = size
             }
         )
+        _state.value = _pizzaState.value
     }
+
     fun selectTopping(page: Int, topping: Int, isSelected: Boolean) {
-        _state.value = _state.value.copy(
-            selectedToppings = _state.value.selectedToppings.toMutableList().apply {
+        _pizzaState.value = _pizzaState.value.copy(
+            selectedToppings = _pizzaState.value.selectedToppings.toMutableList().apply {
                 this[page] = this[page].toMutableList().apply {
                     this[topping] = isSelected
                 }
             }
         )
+        _state.value = _pizzaState.value
     }
 }
